0951f8dce24644cf9dcdcea55622a0a1f6ed5da8
Ayush <133525369+aayushk0250@users.noreply.github.com>
2026-01-17 01:16:41 +0530

    updated project entry

--- learning/src/main/java/com/learning/myFirstProject/MyFirstProjectApplication.java
+++ learning/src/main/java/com/learning/myFirstProject/MyFirstProjectApplication.java
@@ -3,11 +3,10 @@ package com.learning.myFirstProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// only 1 in a project / entry point of a project
 @SpringBootApplication
 public class MyFirstProjectApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
 	}
-
 }
